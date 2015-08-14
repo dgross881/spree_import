eb7d1576ccab98dfc09f687ae7a0e6937fdacadd
dgross881 <dgross881@gmail.com>
2015-08-14 13:03:10 +0800

    fixed bug in engine

--- Rakefile
+++ Rakefile
@@ -8,8 +8,16 @@ task default: :spec
 
 RSpec::Core::RakeTask.new 
 
-APP_RAKEFILE = File.expand_path("../spec/dummy/Rakefile", __FILE__)
+#APP_RAKEFILE = File.expand_path("../spec/dummy/Rakefile", __FILE__)
 load 'rails/tasks/engine.rake'
+
+task :default do
+  if Dir["spec/dummy"].empty?
+    Rake::Task[:test_app].invoke
+    Dir.chdir("../../")
+  end
+  Rake::Task[:spec].invoke
+end
 
 
 desc 'Generates a dummy app for testing'
@@ -17,7 +25,4 @@ task :test_app do
   ENV['LIB_NAME'] = 'spree_import'
   Rake::Task['common:test_app'].invoke
 end
-
-
-
 
--- lib/spree_import/engine.rb
+++ lib/spree_import/engine.rb
@@ -10,13 +10,13 @@ module SpreeImport
       g.test_framework :rspec
     end
     
-   initializer :append_migrations do |app|
-      unless app.root.to_s.match root.to_s
-        config.paths["db/migrate"].expanded.each do |expanded_path|
-          app.config.paths["db/migrate"] << expanded_path
-        end
-      end 
-    end 
+   #initializer :append_migrations do |app|
+      #unless app.root.to_s.match root.to_s
+        #config.paths["db/migrate"].expanded.each do |expanded_path|
+          #app.config.paths["db/migrate"] << expanded_path
+        #end
+      #end 
+    #end 
 
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator*.rb")) do |c|
