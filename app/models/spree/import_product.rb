25a13c14c0087e4c7ce0ec71d852e2662cf5597e
dgross881 <dgross881@gmail.com>
2015-08-14 23:38:47 +0800

    fixed bugs in Spree import model

--- app/models/spree/import_product.rb
+++ app/models/spree/import_product.rb
@@ -1,23 +1,25 @@
 require 'date'
 
-class ImportProduct
-  attr_accessor :name, :description, :slug, :meta_description,
-                :meta_keywords, :meta_keywords, :promotionable,
-                :meta_title, :price, :vendor, :option1, :option2, 
-                :weight, :quantity, :tags, :type, :option_value
+module Spree
+  class ImportProduct
+    attr_accessor :name, :description, :slug, :meta_description,
+                  :meta_keywords, :meta_keywords, :promotionable,
+                  :meta_title, :price, :vendor, :option1, :option2, 
+                  :weight, :quantity, :tags, :type, :option_value
 
-  def initialize(csv_row)
-     @name = csv_row[:name] 
-     @description = csv_row[:description]
-     @slug =  csv_row[:slug] 
-     @meta_description = csv_row[:meta_description] 
-     @meta_keywords = csv_row[:meta_keywords] 
-     @promotionable =  csv_row[:promotionable] 
-     @meta_title = csv_row[:meta_title] 
-     @price = csv_row[:price].to_i
-     @vendor = csv_row[:vendor]
-     @tags = csv[:tags]
-     @type = csv[:type]
-     @option_type = csv[:option_type]
+    def initialize(csv_row)
+       @name = csv_row[:name] 
+       @description = csv_row[:description]
+       @slug =  csv_row[:slug] 
+       @meta_description = csv_row[:meta_description] 
+       @meta_keywords = csv_row[:meta_keywords] 
+       @promotionable =  csv_row[:promotionable] 
+       @meta_title = csv_row[:meta_title] 
+       @price = csv_row[:price].to_i
+       @vendor = csv_row[:vendor]
+       @tags = csv[:tags]
+       @type = csv[:type]
+       @option_type = csv[:option_type]
+    end
   end
-end
+end 
--- app/models/spree/product_import.rb
+++ app/models/spree/product_import.rb
@@ -1,47 +1,49 @@
 require 'csv'
 
-class Spree::ProductImport < ActiveRecord::Base 
-  preference :upload_products, :boolean, :default_false
-  preference :upload_variants, :boolean, :default_false 
+module Spree
+  class ProductImport < ActiveRecord::Base 
+    preference :upload_products, :boolean, :default_false
+    preference :upload_variants, :boolean, :default_false 
 
-  has_attached_file :csv_import, :path => ":rails_root/lib/etc/product_data/data-files/:basename.:extension"
-  validates_attachment :csv_import, presence: true,
-	    :content_type => { content_type: 'text/csv' }
+    has_attached_file :csv_import, :path => ":rails_root/lib/etc/product_data/data-files/:basename.:extension"
+    validates_attachment :csv_import, presence: true,
+        :content_type => { content_type: 'text/csv' }
 
-  def add_products!
-    import_products 
+    def add_products!
+      import_products 
 
-    products = @products_csv.map { |product|  ImportProduct.new(product)  }
+      products = @products_csv.map { |product|  Spree::ImportProduct.new(product)  }
 
-    products.each do |product|
-    product = Spree::Product.create!(name: product.name, description: product.description,
-                                     meta_title: product.meta_title, meta_description: product.meta_description,
-                                     meta_keywords: "#{product.slug}, #{product.name}, the Squirrelz",
-                                     available_on: Time.zone.now, price: product.price,
-                                     shipping_category: Spree::ShippingCategory.find_by!(name: 'Shipping'))
+      products.each do |product|
+      product = Spree::Product.create!(name: product.name, description: product.description,
+                                       meta_title: product.meta_title, meta_description: product.meta_description,
+                                       meta_keywords: "#{product.slug}, #{product.name}, the Squirrelz",
+                                       available_on: Time.zone.now, price: product.price,
+                                       shipping_category: Spree::ShippingCategory.find_by!(name: 'Shipping'))
 
-      product.tag_list = product.tags
-      product.slug = product.slug
-      product.option_types << product.option_type unless product.option_type.nil?
-      # can probably make the same method for properties and option types as taxons 
-      product.properties << product.type unless product.type.nil? 
-      add_product_taxons(product)
-      product.save!
+        product.tag_list = product.tags
+        product.slug = product.slug
+        product.option_types << product.option_type unless product.option_type.nil?
+        # can probably make the same method for properties and option types as taxons 
+        product.properties << product.type unless product.type.nil? 
+        add_product_taxons(product)
+        product.save!
+      end
+    end
+
+    def add_product_taxons(product) 
+     if product.taxon.present? 
+       seperate_taxons = product.taxon.split(",").map(&:strip)
+       taxon = sperate_taxons.map {|product| Spree::Taxon.joins(:translations).find_by(name: product) }
+       product.taxons << taxon unless taxon.nil?
+     end 
+    end 
+
+    private 
+
+    def import_products 
+      options = {headers: true, header_converters: :symbol, skip_blanks: true}
+      @products_csv = CSV.read(self.csv_import.path, options)
     end
   end
-
-  def add_product_taxons(product) 
-   if product.taxon.present? 
-     seperate_taxons = product.taxon.split(",").map(&:strip)
-     taxon = sperate_taxons.map {|product| Spree::Taxon.joins(:translations).find_by(name: product) }
-     product.taxons << taxon unless taxon.nil?
-   end 
-  end 
-
-  private 
-
-  def import_products 
-    options = {headers: true, header_converters: :symbol, skip_blanks: true}
-    @products_csv = CSV.read(self.csv_import.path, options)
-  end
-end
+end 
